a584a66968045adc2f35eadb7ed31a9b99f53eb9
Sahil Patil <sahilpatil9889@gmail.com>
2026-07-27 23:36:44 +0530

    Clean up repository structure

--- dns-test.js
+++
@@ -1,9 +0,0 @@
-const dns = require("dns");
-
-dns.setServers(["8.8.8.8"]);
-
-dns.promises.resolveSrv(
-    "_mongodb._tcp.yt-complete-backend.jubvzoq.mongodb.net"
-)
-.then(console.log)
-.catch(console.error);
